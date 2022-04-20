f11195f9afeaed57e1bdaf5fe3cdd038a44c4391
alvin.wang <alvin.wang@binance.com>
2022-04-20 14:35:11 +0800

    fix: commit acrorn-plugin-guang-keyword

---
+++ examples/acorn-plugin-guang-keyword/index.js
@@ -0,0 +1,14 @@
+const acorn = require("acorn");
+const guangKeywordPlugin = require("./guangKeywordPlugin");
+
+const Parser = acorn.Parser;
+
+const newParser = Parser.extend(guangKeywordPlugin);
+
+var program = `
+    guang
+    const a = 1
+`;
+
+const ast = newParser.parse(program);
+console.log(ast);
